cfa6bba6abeb548e5f0b913ab7f4be33c813a242
Your Name <marielmetalios@gmail.com>
2025-02-25 17:53:42 -0500

    adjusted seed file to .ts for dist

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,6 +1,7 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
 
+
 export default async (modelName: "Question", collectionName: string) => {
   try {
     let modelExists = await models[modelName].db.db!.listCollections({
